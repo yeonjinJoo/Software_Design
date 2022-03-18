2f268942b0aca5bae46d0ba5dbedc711efb15e15
yeonjinjoo <jooyj0529@hanyang.ac.kr>
2022-03-18 14:10:57 +0900

    3-2 assignment

---
+++ 3-2/1/1.py
@@ -0,0 +1,6 @@
+print("Number of cars: ")
+numcar = input()
+print("Number of buses: ")
+numbus = input() 
+total = int(numcar) + int(numbus)
+print("Number of vehicles: " + str(total))
---
+++ 3-2/2/2.py
@@ -0,0 +1,8 @@
+str1 = input()
+str2 = input()
+str3 = input()
+float1 = input()
+float2 = input()
+float3 = input()
+print(str1 + str2 + str3)
+print(float(float1) + float(float2) + float(float3))
